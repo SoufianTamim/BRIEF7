01f944a3332e2fadfcc392a7c5cfcbb2e75a61c5
Soufian Tamim <soufiaintamim22@gmail.com>
2022-11-23 16:50:54 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const name = document.getElementById('name').value;
+const brand = document.getElementById('brand').value;
+const price = document.getElementById('price').value;
+const date = document.getElementById('date').value;
+const type = document.getElementById('type');
+const discount = document.getElementsByName('discount');
+
+
